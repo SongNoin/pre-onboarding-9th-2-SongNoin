b182ba7ba55847fc081ac65c2a320635b2e0393d
SongNoin <as5427072@gmail.com>
2023-03-08 01:59:50 +0900

    style 글로벌테마 설정

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import { ChakraProvider } from "@chakra-ui/react";
+import { theme } from "./components/commons/styles/Theme";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <ChakraProvider resetCSS={true} theme={theme}>
+      <App />
+    </ChakraProvider>
   </React.StrictMode>
 );
