961c86636bc5d271a67f994a606d07bf1ba284d4
SongNoin <as5427072@gmail.com>
2023-03-08 15:16:02 +0900

    feat travelitem 타입 추가

---
+++ src/types/ITravelItem.ts
@@ -0,0 +1,10 @@
+export interface ITreavelItem {
+  idx: number;
+  name: string;
+  mainImage: string;
+  description: string;
+  spaceCategory: string;
+  price: number;
+  maximumPurchases: number;
+  registrationDate: string;
+}
